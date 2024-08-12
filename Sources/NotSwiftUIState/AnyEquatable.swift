54e856f6d7a149cc5432ce51a3363d98481afb16
Ahmad Alhashemi <ahmad@ahmadh.com>
2024-08-12 09:47:50 +0300

    Simplify isEqual implementation

--- Sources/NotSwiftUIState/AnyEquatable.swift
+++ Sources/NotSwiftUIState/AnyEquatable.swift
@@ -1,25 +1,9 @@
 // Check if the two values are Equatable and equal
 func isEqual(_ lhs: Any, _ rhs: Any) -> Bool {
-    func f<LHS>(lhs: LHS) -> Bool {
-        if let typeInfo = Wrapped<LHS>.self as? AnyEquatable.Type {
-            return typeInfo.isEqual(lhs: lhs, rhs: rhs)
-        }
-        return false
+    guard let lhs = lhs as? any Equatable else { return false }
+    func f<LHS: Equatable>(_ lhs: LHS) -> Bool {
+        guard let rhs = rhs as? LHS else { return false }
+        return lhs == rhs
     }
-    return _openExistential(lhs, do: f)
-}
-
-protocol AnyEquatable {
-    static func isEqual(lhs: Any, rhs: Any) -> Bool
-}
-
-enum Wrapped<T> { }
-
-extension Wrapped: AnyEquatable where T: Equatable {
-    static func isEqual(lhs: Any, rhs: Any) -> Bool {
-        guard let l = lhs as? T, let r = rhs as? T else {
-            return false
-        }
-        return l == r
-    }
+    return f(lhs)
 }
